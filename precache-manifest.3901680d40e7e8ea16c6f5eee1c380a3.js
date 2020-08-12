102d793da5408d2b5a204cef1e51ca9aa8275670
G-TRITON <braj85216@gmail.com>
2020-08-12 15:58:00 +0530

    Updates

---
+++ precache-manifest.3901680d40e7e8ea16c6f5eee1c380a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b84ff318ff32262d26d5fe78ecadc560",
+    "url": "/CelebFaceRecog/index.html"
+  },
+  {
+    "revision": "3f017869d3ee8c7f7224",
+    "url": "/CelebFaceRecog/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f123ff79557d9d89194a",
+    "url": "/CelebFaceRecog/static/css/main.83e2b402.chunk.css"
+  },
+  {
+    "revision": "3f017869d3ee8c7f7224",
+    "url": "/CelebFaceRecog/static/js/2.b1f0bf99.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/CelebFaceRecog/static/js/2.b1f0bf99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f123ff79557d9d89194a",
+    "url": "/CelebFaceRecog/static/js/main.76f60780.chunk.js"
+  },
+  {
+    "revision": "f39b70acd545a3dce72b",
+    "url": "/CelebFaceRecog/static/js/runtime-main.663b5d70.js"
+  }
+]);
